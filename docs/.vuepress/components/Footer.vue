47165c8240e490e4dc1ce5a3db1dcde013502109
linyuxuanlin <824676271@qq.com>
2020-03-14 15:48:32 +0800

    Footer test

--- docs/.vuepress/components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer>
-    <a href="http://www.beian.miit.gov.cn/">粤 ICP 备 20014898 号</a>
-  </footer>
-</template>
-
-<style lang="stylus" scoped>
-// css/stylus here or in global stylus file
-</script>
