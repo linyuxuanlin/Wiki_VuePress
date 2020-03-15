d3bf93b7a50e6f2cff3edfa9ebfa99ccc643b202
linyuxuanlin <824676271@qq.com>
2020-03-15 16:18:57 +0800

    尝试添加备案 footer

---
+++ docs/.vuepress/components/Footer.vue
@@ -0,0 +1,3 @@
+ <p class="cyber-security" v-if="$themeConfig.cyberSecurityRecord">
+      <img src="https://img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png" alt="">
+      <a :href="$themeConfig.cyberSecurityLink || '#'">{{ $themeConfig.cyberSecurityRecord }}</a>
