63f692b92945dcc279487657a1204c4f33e7b7c9
linyuxuanlin <824676271@qq.com>
2020-03-26 12:51:39 +0800

    test2

--- config/pluginConf.js
+++ config/pluginConf.js
@@ -1,17 +1,20 @@
 module.exports = [
-    ['@vuepress/back-to-top'], // 回到顶部
-    ['reading-progress'], // 阅读进度条插件
-    ['vuepress-plugin-code-copy', true], // 代码块一键复制按钮
-    ['@vuepress/google-analytics', // Google 分析
-        {
-            'ga': 'UA-152900803-1'
-        }
-    ],
-    ['@vuepress/pwa',
-        {
-            serviceWorker: true,
-            popupComponent: 'MySWUpdatePopup',
-            updatePopup: true
-        }
-    ]
+  ["@vuepress/back-to-top"], // 回到顶部
+  ["latex"],
+  ["reading-progress"], // 阅读进度条插件
+  ["vuepress-plugin-code-copy", true], // 代码块一键复制按钮
+  [
+    "@vuepress/google-analytics", // Google 分析
+    {
+      ga: "UA-152900803-1"
+    }
+  ],
+  [
+    "@vuepress/pwa",
+    {
+      serviceWorker: true,
+      popupComponent: "MySWUpdatePopup",
+      updatePopup: true
+    }
+  ]
 ];
