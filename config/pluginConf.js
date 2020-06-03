247a04bbb7601bc5cc12ac7d6277bde82ef72155
linyuxuanlin <824676271@qq.com>
2020-06-03 15:04:22 +0800

    还是开启PWA吧

--- config/pluginConf.js
+++ config/pluginConf.js
@@ -24,16 +24,16 @@ module.exports = [
     }
   ],
 
-
+*/
   [
     "@vuepress/pwa",
     {
-      serviceWorker: false, //用于缓存页面的内容以供离线使用
+      serviceWorker: true, //用于缓存页面的内容以供离线使用
       popupComponent: "MySWUpdatePopup",
       updatePopup: true,
     },
   ],
- */
+
   [
     "vuepress-plugin-rss",
     {
