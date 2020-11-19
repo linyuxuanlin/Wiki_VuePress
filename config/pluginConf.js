57d1d197c1893de1aa0f913197ebc17569626986
linyuxuanlin <824676271@qq.com>
2020-11-19 21:02:41 +0800

    close code-copy

--- config/pluginConf.js
+++ config/pluginConf.js
@@ -2,7 +2,7 @@ module.exports = [
   ["@vuepress/back-to-top"], // 回到顶部
 
   ["reading-progress"], // 阅读进度条插件
-  ["vuepress-plugin-code-copy", true], // 代码块一键复制按钮
+  ["vuepress-plugin-code-copy", false], // 代码块一键复制按钮
   [
     "@vuepress/google-analytics", // Google 分析
     {
