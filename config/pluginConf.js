562f44ce3531e006e7e5e55ee5b215cf18aaed01
linyuxuanlin <824676271@qq.com>
2020-11-20 08:24:40 +0800

    asdasdasd

--- config/pluginConf.js
+++ config/pluginConf.js
@@ -28,7 +28,7 @@ module.exports = [
   [
     "@vuepress/pwa",
     {
-      serviceWorker: true, //用于缓存页面的内容以供离线使用
+      serviceWorker: false, //用于缓存页面的内容以供离线使用
       popupComponent: "MySWUpdatePopup",
       updatePopup: false,
     },
