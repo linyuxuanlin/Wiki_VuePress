4aa7f76df8a072d5606d20137b42c6cd92d39b95
linyuxuanlin <824676271@qq.com>
2020-11-22 20:57:01 +0800

    update friend-links

--- config/navConf.js
+++ config/navConf.js
@@ -1,16 +1,22 @@
 module.exports = [
+  /*
   {
     text: "Home",
     link: "/",
   },
-  /*
+  
     {
         text: 'Tools',
         link: '/Tools'
     },
     */
+
   {
-    text: "Collection",
+    text: "友链",
+    link: "/Links",
+  },
+  {
+    text: "收藏夹",
     link: "/Collection",
   },
 ];
